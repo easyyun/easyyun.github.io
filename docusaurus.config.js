553ca5c006cf9c40a73cfb860ece2ee13ab2878b
lifei <lifei@lifeideMacBook-Pro.local>
2023-02-20 22:19:03 +0800

    doc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -94,9 +94,9 @@ const config = {
         style: 'light',
 
         copyright: `Copyright © 2021 - ${new Date().getFullYear()} Easyyun, Inc. 
-        <a href="/privacy/privacy.html" target="_blank">隐私</a>  | <a href="/privacy/service.html" target="_blank">服务</a> 
-        <br /><a href="http://beian.miit.gov.cn/" target="_blank"><small>浙ICP备19040696号-6</small></a> | 
-            <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33010802012279" target="_blank">
+        <a href="/privacy/privacy.html" target="_blank" rel="noopener noreferrer">隐私</a>  | <a href="/privacy/service.html" target="_blank" rel="noopener noreferrer">服务</a> 
+        <br /><a href="http://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer"><small>浙ICP备19040696号-6</small></a> | 
+            <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33010802012279" target="_blank" rel="noopener noreferrer">
             <img src="/img/ba.png"/> <small>浙公网安备 33010802012279号</small></a>`,
       },
       prism: {
